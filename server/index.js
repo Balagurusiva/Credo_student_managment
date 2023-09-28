1b69088e8b67b08646a700a21f25b45d1e29cc3c
Balaguru <balagurusiva2002@gmail.com>
2023-09-28 16:56:06 +0530

    modified:   server/.gitignore 	modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
-import express  from "express";
+import express from "express";
+import { PORT, mongoDBURL } from './config.js'
+import mongoose from "mongoose";
 
 const app = express()
 
-app.use('/', (req,res) => {
+app.use('/', (req, res) => {
     res.send("<p>hello</p>")
 })
 
-app.listen(5000, console.log("listening at 5000"))
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        app.listen(PORT, console.log("listening at 5000"))
+    })
+    .catch((error)=>{
+       console.log(error)
+    })
